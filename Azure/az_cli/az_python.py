3528160c242d404193b647c642c032d87fffc9ad
Manny <mberrueta@microsoft.com>
2021-12-02 15:36:27 -0800

    Added some more links

--- Azure/az_cli/az_python.py
+++ Azure/az_cli/az_python.py
@@ -1,2 +1,11 @@
+#!/usr/bin/env python3
+import az_cli
+
+
+# Packages required
+#   azure-mgmt-resource>=18.0.0
+#   azure-identity>=1.5.0
 # https://stackoverflow.com/questions/51546073/how-to-run-azure-cli-commands-using-python
 # https://docs.microsoft.com/en-us/azure/developer/python/configure-local-development-environment?tabs=cmd
+# https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-example-list-resource-groups\
+# https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-authenticate
