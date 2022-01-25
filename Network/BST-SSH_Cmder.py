9559fe1ea22d5519d6629748abd9a2c70834b0c2
Manny <mberrueta@microsoft.com>
2022-01-24 16:08:09 -0800

    Working on Secure File Transfer (SCP) feature for transfering files to and from.

--- Network/BST-SSH_Cmder.py
+++ Network/BST-SSH_Cmder.py
@@ -1,5 +1,8 @@
 from sys import stderr, stdout
+import os.path
+import asyncio
 import paramiko
+from scp import SCPClient
 import argparse
 import getpass
 import csv
@@ -26,6 +29,12 @@ parser.add_argument("--ip", type=str, required=False,
 parser.add_argument("--port", type=int, required=False,
                     default=22, help="--port 22")
 parser.add_argument(
+    "--scp", type=str, required=False, default="", help="--scp put or --scp get"
+)
+parser.add_argument(
+    "--scp_path", type=list, required=False, default=[], help="--scp_path target/File/Path1 target/File/Path2"
+)
+parser.add_argument(
     "--ip_list", type=str, required=False, default="", help="--ip_list my_ip_list.txt"
 )
 parser.add_argument("--cmd", type=str, required=False,
@@ -38,8 +47,28 @@ parser.add_argument(
     help="--cmd_script file_w_commands.sh",
 )
 
-
 args = parser.parse_args()
+
+
+def scp_transfer(user: str, password: str, key: str, ip_list: str, port: str, scp_action: str, paths: list):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    for ip in ip_list:
+        if password != "" and key == "":
+            ssh_client.connect(ip, port, user, password)
+        elif key != "" and password == "":
+            ssh_client.connect(
+                ip, port, user, key_filename=key, look_for_keys=False)
+
+        scp = SCPClient(ssh_client.get_transport())
+
+        # Check if file(s) exists if it does not exist
+        for file in paths:
+            if not os.path.isfile(file):
+                paths.remove(file)
+
+        scp.put(paths)
 
 
 def ssh_cmder(
@@ -79,6 +108,8 @@ def main():
     ip_list = []
     port = args.port
     cmd_list = []
+    scp_action = args.scp
+    scp_paths = args.scp_path
 
     if args.ip_list == "" and args.ip == "":
         print("You must provide either --ip or --ip_list as an argument")
@@ -103,24 +134,6 @@ def main():
         print("ERROR: Missing ip or ip list")
         exit(1)
 
-    if args.cmd == "" and args.cmd_script == "":
-        print("You must provide either --cmd or --cmd_script as an argument")
-        exit(1)
-    elif args.cmd != "" and args.cmd_script != "":
-        print(
-            "You must provide either --cmd or --cmd_script as an argument but NOT both!"
-        )
-        exit(1)
-    elif args.cmd_script:
-        with open(args.cmd_script) as cmd_script_file:
-            # cmd_script_file_content = cmd_script_file.read().splitlines()
-            cmd_list = cmd_script_file.read().splitlines()
-    elif args.cmd:
-        cmd_list.append(args.cmd)
-    else:
-        print("ERROR: Missing cmd or cmd script")
-        exit(1)
-
     if args.pw == "" and args.key == "":
         print("You must provide either --pw or --key as an argument")
         exit(1)
@@ -135,7 +148,37 @@ def main():
         print("ERROR: Missing pw or key file")
         exit(1)
 
-    ssh_cmder(user, pwd, key_path, ip_list, port, cmd_list)
+    if scp_action != "":
+        if not scp_paths:
+            print("=== Error: No paths passed for scp file transfer ===")
+            exit(1)
+
+        if scp_action != "put" or scp_action != "get":
+            print("=== Error: --scp-action must be either 'put' or 'get'  ===")
+            exit(1)
+
+        scp_transfer(user, pwd, key_path, ip_list, port, scp_action, scp_paths)
+
+    else:
+        if args.cmd == "" and args.cmd_script == "":
+            print("You must provide either --cmd or --cmd_script as an argument")
+            exit(1)
+        elif args.cmd != "" and args.cmd_script != "":
+            print(
+                "You must provide either --cmd or --cmd_script as an argument but NOT both!"
+            )
+            exit(1)
+        elif args.cmd_script:
+            with open(args.cmd_script) as cmd_script_file:
+                # cmd_script_file_content = cmd_script_file.read().splitlines()
+                cmd_list = cmd_script_file.read().splitlines()
+        elif args.cmd:
+            cmd_list.append(args.cmd)
+        else:
+            print("ERROR: Missing cmd or cmd script")
+            exit(1)
+
+        ssh_cmder(user, pwd, key_path, ip_list, port, cmd_list)
 
 
 if __name__ == "__main__":
