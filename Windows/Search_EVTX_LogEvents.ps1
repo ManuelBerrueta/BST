0e44c4e93faadfac67ef44710f44fb7a21508ba7
unknown <manuelberrueta@gmail.com>
2022-07-19 11:56:38 -0700

    Added the -TargetEvtxFile parameter for the Search_EVTX_LogEvents.ps1 and some minor typos :)

--- Windows/Search_EVTX_LogEvents.ps1
+++ Windows/Search_EVTX_LogEvents.ps1
@@ -1,11 +1,15 @@
-<# Script to search through Windows Event Logs .evtx files
-                            by 
-#>                     Manuel Berrueta
+<# 
+    Script to search through Windows Event Logs .evtx files
+                            by
+                      Manuel Berrueta
+#>
 
-# Change this to the path of the evtx file
-$targetEvtxFile = "C:\temp\myEvents.evtx"
+[CmdletBinding()]
+param(
+    [string] $TargetEvtxFile
+)
 
-# The following events were sourced from DeepBlue (https://github.com/sans-blue-team/DeepBlueCLI)
+# The following events were sourced from DeepBlueCLI (https://github.com/sans-blue-team/DeepBlueCLI)
 $sys_events = @("7030", "7036", "7045", "7040", "104")
 $sec_events = @("4688", "4672", "4720", "4728", "4732", "4756", "4625", "4673", "4674", "4648", "1102")
 $app_events = @("2")
@@ -14,14 +18,13 @@ $powershell_events = @("4103", "4104")
 $sysmon_events = @("1", "7")
 $testEventNum = @("1033", "1107")
 
-$Events = Get-WinEvent -Path $targetEvtxFile
+$Events = Get-WinEvent -Path $TargetEvtxFile
 
 #!This is the string that will be matched in message
 $interestedStr = "client"
 
 #!This is a list of strings intersting to us
 $interestingStrList = @("client", "user", "password")
-
 
 ForEach ($Event in $Events) {
     $eventID = $Event.id
@@ -31,8 +34,15 @@ ForEach ($Event in $Events) {
     if ( $eventIDStr | ? { $testEventNum -match $_ }  ) {
         $eventType = $Event.LogName
         #echo "$eventID is a $eventType event"
+
+        #! Uncomment whatever properties you would like to see
         #$Event.Properties
         #$Event.Message
+        #$Event.MachineName
+        #$Event.TimeCreated
+        #$Event.UserId
+        #$Event.ProcessId
+        #$Event.ThreadId
     }
 
     #* Match 1 string of interests in the message
@@ -40,7 +50,7 @@ ForEach ($Event in $Events) {
         Write-Host $Event.Message -ForegroundColor Green -BackgroundColor Black
     }
     
-    # Match list of strings of interests in the message
+    #* Match list of strings of interests in the message
     if ( $Event.Message | Select-String -Pattern $interestingStrList  ) {
         Write-Host $Event.Message -ForegroundColor Green -BackgroundColor Black
     }
