b6d0b8114a060af6333531d900c607e6169baf06
manuelberrueta <manuelberrueta@gmail.com>
2022-12-10 13:38:28 -0800

    Updated Search_EVTX_LogEvents.ps1 to show more useful information in a clearer and more compact manner

--- Windows/Search_EVTX_LogEvents.ps1
+++ Windows/Search_EVTX_LogEvents.ps1
@@ -18,13 +18,14 @@ $powershell_events = @("4103", "4104")
 $sysmon_events = @("1", "7")
 $testEventNum = @("1033", "1107")
 
+$TargetEvtxFile = "C:\Users\0siris\Downloads\2022_KringleCon\powershell.evtx"
 $Events = Get-WinEvent -Path $TargetEvtxFile
 
 #!This is the string that will be matched in message
 $interestedStr = "client"
 
 #!This is a list of strings intersting to us
-$interestingStrList = @("client", "user", "password")
+$interestingStrList = @("admin", "password", "secret")
 
 ForEach ($Event in $Events) {
     $eventID = $Event.id
@@ -36,22 +37,34 @@ ForEach ($Event in $Events) {
         #echo "$eventID is a $eventType event"
 
         #! Uncomment whatever properties you would like to see
-        #$Event.Properties
-        #$Event.Message
+        $Event.Properties
+        $Event.Message
         #$Event.MachineName
-        #$Event.TimeCreated
+        $Event.TimeCreated
         #$Event.UserId
         #$Event.ProcessId
         #$Event.ThreadId
     }
 
     #* Match 1 string of interests in the message
-    if ( $Event.Message -like "*$interestedStr*") {
-        Write-Host $Event.Message -ForegroundColor Green -BackgroundColor Black
-    }
+    #if ( $Event.Message -like "*$interestedStr*") {
+    #    Write-Host $Event.Message -ForegroundColor Green -BackgroundColor Black
+    #}
     
     #* Match list of strings of interests in the message
     if ( $Event.Message | Select-String -Pattern $interestingStrList  ) {
+        Write-Host "`n" -BackgroundColor DarkBlue -n
+        Write-Host
+        Write-Host " $($Event.TimeCreated) " -ForegroundColor White -BackgroundColor Magenta -n
+        Write-Host "| EVENT ID: $($Event.Id) " -ForegroundColor Blue -n
+        Write-Host "| Machine: $($Event.MachineName) " -ForegroundColor Blue -n
+        Write-Host "| User: $($Event.UserId) " -ForegroundColor Blue -n
+        Write-Host "| PID: $($Event.ProcessId)" -ForegroundColor Blue -n
+        Write-Host
+        Write-Host "TASK: $($Event.TaskDisplayName)" -ForegroundColor Yellow
         Write-Host $Event.Message -ForegroundColor Green -BackgroundColor Black
+        Write-Host "Event Properties:`n$($Event.Properties[0].Value)" -ForegroundColor Red
+        Write-Host "`n" -BackgroundColor DarkBlue -n
+        Write-Host ""
     }
 }
