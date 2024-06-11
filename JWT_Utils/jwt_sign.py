915869651e8446753dd87a8b4d59c731f7a4eb07
ManuelBerrueta <manuelberrueta@gmail.com>
2024-06-11 15:10:54 -0700

    Updated jwt_sign.py to be more CLI friendly, as well as being able to be imported as a module

--- JWT_Utils/jwt_sign.py
+++ JWT_Utils/jwt_sign.py
@@ -2,47 +2,60 @@ from cryptography.hazmat.primitives import serialization
 import jwt
 import datetime
 import argparse
+import pyperclip
 
-# To run this script: python jwt_sign.py /path/to/private/key
+# NOTE: you can run this script from the command line with the following command:
+# 1. python3 jwt_sign.py path/to/private_key.pem --x5t x5t_value --aud aud_claim --iss client_id --sub client_id
+# 2. python3 jwt_sign.py path/to/private_key.pem, and follow the prompts for each of the inputs
 
-parser = argparse.ArgumentParser(description='CLI input')
-parser.add_argument('private_key_path', type=str, help='The path to the private key')
-args = parser.parse_args()
+# NOTE: You can also use it as a module by importing the function and calling it with the required arguments
+# from jwt_sign import sign_jwt
+# token = sign_jwt(private_key_path, x5t, aud, iss, sub) or 
+# token = sign_jwt('/path/to/private/key', x5t='your_x5t', aud='your_aud', iss='your_iss', sub='your_sub')
+# print(f'JWT Token: {token}')
 
-# Now you can use args.private_key_path where you need the private key path
-print(f'Using Private key @ path: {args.private_key_path}')
+def sign_jwt(private_key_path, x5t=None, aud=None, iss=None, sub=None):
+    # Open + read the private key
+    private_key_str = open(private_key_path, 'r').read()
 
-private_key_path = args.private_key_path
+    # Load as PEM private key
+    private_key = serialization.load_pem_private_key(
+        private_key_str.encode('utf-8'), password=None)  # password=b''
 
-# Open + read the private key
-private_key_str = open(args.private_key_path, 'r').read()
+    # Use provided arguments or prompt for input
+    x5t = x5t if x5t is not None else input('Enter the x5t: ')
+    aud = aud if aud is not None else input('Enter the aud claim: ')
+    iss = iss if iss is not None else input('Enter the iss claim: ')
+    sub = sub if sub is not None else input('Enter the sub claim: ')
 
-# Load as PEM private key
-private_key = serialization.load_pem_private_key(
-    private_key_str.encode('utf-8'), password=None) #password=b''
+    headers = {
+        "alg": "RS256",
+        "typ": "JWT",
+        "x5t": x5t
+    }
 
-headers = {
-    "alg": "RS256",
-    "typ": "JWT",
-    "x5t": input('Enter the x5t: ')
-}
+    payload_data = {
+        "aud": aud,
+        "iss": iss,
+        "nbf": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=30),
+        "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=2),
+        "sub": sub
+    }
 
-payload_data = {
-    "aud": input('Enter the aud claim: '),
-    "iss": input('Enter the iss claim: '),
-    "nbf": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(seconds=30),
-    "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=2),
-    "sub": input('Enter the sub claim: ')
-}
+    # Sign the JWT
+    token = jwt.encode(payload=payload_data, key=private_key, algorithm="RS256", headers=headers)
+    return token
 
-new_token = jwt.encode(
-    payload=payload_data,
-    headers=headers,
-    key=private_key,
-    algorithm='RS256'
-)
-print(new_token)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='CLI input for JWT signing')
+    parser.add_argument('private_key_path', type=str, help='The path to the private key')
+    parser.add_argument('--x5t', type=str, help='x5t value', default=None)
+    parser.add_argument('--aud', type=str, help='aud claim', default=None)
+    parser.add_argument('--iss', type=str, help='iss claim', default=None)
+    parser.add_argument('--sub', type=str, help='sub claim', default=None)
+    args = parser.parse_args()
 
-# Reference:
-# - [How to handle JWTs](https://auth0.com/blog/how-to-handle-jwt-in-python/
-# - [PyJWT Docs](https://pyjwt.readthedocs.io/en/stable/usage.html#encoding-decoding-tokens-with-rs256-rsa)
+    # Call the function with command line arguments or prompts
+    token = sign_jwt(args.private_key_path, args.x5t, args.aud, args.iss, args.sub)
+    pyperclip.copy(token)
+    print(f'JWT Token (Copied to Clipboard):\n{token}')
