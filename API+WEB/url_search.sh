98b5ad3952a69ef22a6cb0f5d4eac5bc2faadac5
ManuelBerrueta <manuelberrueta@gmail.com>
2024-12-12 06:55:54 -0800

    Updates to url_search.sh

--- API+WEB/url_search.sh
+++ API+WEB/url_search.sh
@@ -1,3 +1,14 @@
+# This script searches for URLs in a specified target (file or directory).
+# Usage: ./url_search.sh <target>
+# 
+# Arguments:
+#   <target> - The file or directory to search for URLs.
+#
+# If the target is a directory, it recursively searches for URLs in all files within the directory.
+# If the target is a file, it searches for URLs within the file.
+# 
+# The script uses egrep with a regex pattern to match URLs and highlights them in the output.
+
 # Check if target_dir argument is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <target>"
