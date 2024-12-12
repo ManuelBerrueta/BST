f95a96d09a68194e973594d695c5c6be9e1924de
ManuelBerrueta <manuelberrueta@gmail.com>
2024-12-12 06:57:41 -0800

    Added az-vm scripts

---
+++ Azure/az-vm/Get-IMDSInstance.ps1
@@ -0,0 +1,1 @@
+Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -NoProxy -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | ConvertTo-Json -Depth 64
---
+++ Azure/az-vm/Get-TokenIMDS.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -Headers @{Metadata="true"}
